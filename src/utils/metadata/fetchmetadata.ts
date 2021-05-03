056fc749dcc23e51fcaf136599c1caea3d6b7e80
Andrew Jarvis <33203802+ajarvis3@users.noreply.github.com>
2021-05-03 18:45:11 -0400

    Blah

--- src/utils/metadata/fetchmetadata.ts
+++ src/utils/metadata/fetchmetadata.ts
@@ -5,14 +5,13 @@ import MetaDataBuilder from "./types/metadatabuilder";
 
 // maybe add image width and height too
 
-
 const fetchMetaData = (url: string) => {
    const search = [
-     "og:title",
-     "og:site_name",
-     "og:url",
-     "og:description",
-     "og:image",
+      "og:title",
+      "og:site_name",
+      "og:url",
+      "og:description",
+      "og:image",
    ];
 
    // switch this to env at some point
@@ -29,15 +28,16 @@ const fetchMetaData = (url: string) => {
          if (html) {
             const builder = new MetaDataBuilder();
 
-
             const dom = new JSDOM(html);
             const document = dom.window.document;
+            // doesn't work if meta tags generated dynamically
+            // https://www.thekitchn.com/sweet-corn-blueberry-muffins-23048122
             document.querySelectorAll("meta").forEach((element) => {
                const index = search.indexOf(element.getAttribute("property"));
                if (index !== -1) {
-                 const propertyName = element.getAttribute("property");
-                 const value = element.getAttribute("content");
-                 builder.setProperty(propertyName, value);
+                  const propertyName = element.getAttribute("property");
+                  const value = element.getAttribute("content");
+                  builder.setProperty(propertyName, value);
                }
             });
             console.log(builder.build());
