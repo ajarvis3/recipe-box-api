d1adf81af2969092f71f82f7d63d5fa23d79712e
Andrew Jarvis <33203802+ajarvis3@users.noreply.github.com>
2021-05-10 16:43:37 -0400

    Remove comments

--- src/utils/auth/hashing.ts
+++ src/utils/auth/hashing.ts
@@ -4,16 +4,3 @@ const hashPassword = (password: string, salt: string) => {
    return crypto.pbkdf2Sync(password, salt, 1000, 32, "sha512").toString("hex");
 };
 export default hashPassword;
-
-// rule of threes: use three times, make it a function
-// avoid lots of params
-// short functions
-// encapsulating logic for some routes
-// create a class that handles users in database
-// consistency
-
-// refactor existing code BEFORE building new things
-// do some planning about dashboard
-// fetch meta data for next week
-//    information is standardized
-//    identify popular websites, check to see that it works
